b71115cb4d0b4584853355d00e707f1fb2b20664
Noorain YourTechBud <noorain@spaceuptech.com>
2020-03-05 18:30:38 +0530

    updated doubler to speak to adder over the network

--- basic-service/index.js
+++ basic-service/index.js
@@ -1,26 +1,41 @@
 var express = require("express");
 var app = express();
 
+const axios = require('axios').default;
+
 app.use(express.json());
 
-app.get("/add/:num1/:num2", function (req, res) {
- const num1 = Number(req.params.num1);
- const num2 = Number(req.params.num2);
- const response = { value: num1 + num2 };
- res.status(200).send(JSON.stringify(response));
+const adderURL = process.env.ADDER_URL || 'localhost'
+
+app.get("/add/:num1/:num2", (req, res) => {
+  const num1 = Number(req.params.num1);
+  const num2 = Number(req.params.num2);
+  const response = { value: num1 + num2 };
+  res.status(200).send(JSON.stringify(response));
 });
 
-app.post("/double", function (req, res) {
- const num = Number(req.body.value);
- const response = { value: num * 2 };
- res.status(200).send(JSON.stringify(response));
+app.post("/double", async (req, res) => {
+  // Get the number from the body
+  const num = Number(req.body.value);
+
+  // Generate the url
+  const url = `http://${adderURL}:8080/add/${num}/${num}`
+
+  try {
+    // Fire request and get response
+    const response = await axios.get(url);
+    // Send the response to the client
+    res.status(200).send(JSON.stringify(response.data));
+  } catch (e) {
+    res.status(500).send(JSON.stringify({ error: "an error occured" }));
+  }
 });
 
-app.post("/logger", function (req, res) {
- console.log("Received an event:\n", JSON.stringify(req.body, null, 2))
- res.status(200).send(JSON.stringify({}));
+app.post("/logger", (req, res) => {
+  console.log("Received an event:\n", JSON.stringify(req.body, null, 2))
+  res.status(200).send(JSON.stringify({}));
 });
 
-var server = app.listen(8080, function () {
- console.log("app running on port:", server.address().port);
+var server = app.listen(8080, () => {
+  console.log("app running on port:", server.address().port);
 });
